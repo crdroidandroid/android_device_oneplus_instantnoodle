835af48015beaf00cb0713cfa7f4c70adf81f593
firebird11 <hbgassel@gmail.com>
2021-02-01 19:50:44 +0200

    instantnoodle: use Coral fingerprint
    Change-Id: Iaa42a65238914e301feeeba3a53d48de1b527508

--- lineage_instantnoodle.mk
+++ lineage_instantnoodle.mk
@@ -35,6 +35,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus8 \
     PRODUCT_NAME=OnePlus8_EEA \
-    PRIVATE_BUILD_DESC="OnePlus8-user 11   release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210105.003 7005429 release-keys"
 
-BUILD_FINGERPRINT := OnePlus/OnePlus8_EEA/OnePlus8:11/RP1A.201005.001/2012102310:user/release-keys
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys
