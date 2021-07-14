f82a7d6660c0380b449d4d5bdc0e16c41a852d97
firebird11 <hbgassel@gmail.com>
2021-07-14 10:29:42 +0300

    instantnoodle: Update to redfin July 2021 fingerprint
    Change-Id: I7377e8ebdc1142dd8cbb5a3c99d8d5d3201d9702

--- lineage_instantnoodle.mk
+++ lineage_instantnoodle.mk
@@ -37,4 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus8_EEA \
     PRIVATE_BUILD_DESC="OnePlus8-user 11   release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
